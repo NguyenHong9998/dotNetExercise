dde46ef7bbc34f6211b451842649235d651fac9c
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-10 21:58:43 +0700

    add exercise07

---
+++ Exercise.Test/Exercise07Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise07Test
+    {
+        [TestMethod]
+        public void TesFact()
+        {
+            Assert.AreEqual(new Exercise07().Factorial(1), 1);
+            Assert.AreEqual(new Exercise07().Factorial(5), 120);
+            Assert.AreEqual(new Exercise07().Factorial(0), 1);
+        }
+    }
+}
---
+++ Exercise/Exercise07.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise07
+    {
+        public int Factorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
