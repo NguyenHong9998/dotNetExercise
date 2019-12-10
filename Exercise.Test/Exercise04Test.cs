98ffc65dcb6eb0197899ed6f458540d3d5c0ad1c
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-10 21:37:34 +0700

    add exercise04

---
+++ Exercise.Test/Exercise04Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise04Test
+    {
+        [TestMethod]
+        public void TestFibonaci()
+        {
+            Assert.AreEqual(new Exercise04().PrintFibonaci(9), "1 1 2 3 5 8");
+            Assert.AreEqual(new Exercise04().PrintFibonaci(20), "1 1 2 3 5 8 13");
+            Assert.AreEqual(new Exercise04().PrintFibonaci(100), "1 1 2 3 5 8 13 21 34 55 89");
+        }
+    }
+}
---
+++ Exercise/Exercise04.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise04
+    {
+        // Hàm tính số fibonaci thứ n
+        public int FibonaciAtN(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            return FibonaciAtN(n - 1) + FibonaciAtN(n - 2);
+        }
+        //xuất ra danh sách những số fibonaci không lớn hơn n.
+        public String PrintFibonaci(int number)
+        {
+            List<int> list = new List<int>();
+            int i = 1;
+            while (FibonaciAtN(i) < number)
+            {
+                list.Add(FibonaciAtN(i));
+                i++;
+            }
+            return String.Join(" ", list.ToArray());
+        }
+    }
+}
