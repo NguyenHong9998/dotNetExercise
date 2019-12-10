ba5629cc2d0a11759c837aa4e81079e795ba66b5
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-10 21:34:56 +0700

    add exercise03

---
+++ Exercise.Test/Exercise03Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise03Test
+    {
+        [TestMethod]
+        public void TestDissectNumber()
+        {
+            Assert.AreEqual(new Exercise03().DissectNumber(12), "2*2*3");
+            Assert.AreEqual(new Exercise03().DissectNumber(6), "2*3");
+        }
+    }
+}
---
+++ Exercise/Exercise03.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise03
+    {
+        public String DissectNumber(int number)
+        {
+            List<int> list = new List<int>();
+            String result = "";
+            for (int i = 2; i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    list.Add(i);
+                    number /= i;
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == list.Count - 1)
+                {
+                    result += list.ElementAt(i);
+                }
+                else
+                {
+                    result += list.ElementAt(i) + "*";
+                }
+            }
+            return result;
+        }
+
+    }
+}
