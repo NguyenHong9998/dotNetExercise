4b1671fea40f0dc7f06bc8fcbcad18e279b3a45f
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-10 21:54:48 +0700

    add exercise06

---
+++ Exercise.Test/Exercise06Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Excercise06Test
+    {
+        [TestMethod]
+        public void TestSumSequenceOfNumbers()
+        {
+            Assert.AreEqual(new Exercise06().SumSequenceOfNumbers(4), 40);
+            Assert.AreEqual(new Exercise06().SumSequenceOfNumbers(6), 112);
+        }
+    }
+}
---
+++ Exercise/Exercise06.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise06
+    {
+        public int SumSequenceOfNumbers(int number)
+        {
+            var sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i * (i + 1);
+            }
+            return sum;
+        }
+    }
+}
