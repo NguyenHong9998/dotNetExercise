603fe78063830a3c692aaaeedfcb747f44466ca1
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-10 21:32:57 +0700

    add exercise02

---
+++ Exercise/Exercise02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise02
+    {
+        public int SumDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
